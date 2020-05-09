b3fc7e7ba7988f12f6f7786d319ae53f6a7f0ab1
Vladislav <vladislav.s@nyblecraft.com>
2020-05-10 00:07:34 +0300

    created frame_layout

--- app/src/main/java/com/example/lesson1java/MainActivity.java
+++ app/src/main/java/com/example/lesson1java/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.passport_const);
+        setContentView(R.layout.frame_layout);
     }
 }
